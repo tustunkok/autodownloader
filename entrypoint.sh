9484a5958f211eca19a2988f05d25421d751c106
Tolga Üstünkök <tolgaustunkok@gmail.com>
2026-05-30 15:02:26 +0300

    Fix OpenVPN variant: add TUN device mapping, iproute2, fix log path, ignore block-outside-dns

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,11 @@
 
 openvpn --daemon ovpn \
         --status /tmp/openvpn.status \
-        --log-append /logs/openvpn.log \
+        --log-append /app/logs/openvpn.log \
         --config /etc/openvpn/autodownloader.conf
 
 for i in $(seq 1 60); do
-    if ip route show default | grep -q tun0; then
+    if ip route show | grep tun0; then
         echo "OpenVPN connected"
         break
     fi
